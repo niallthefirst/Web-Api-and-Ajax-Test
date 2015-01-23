52ed0b61f2a972ca1eeb88920df82407b42edc69
Niall Fallon <nfallon2002@gmail.com>
2015-01-23 17:04:41 +0000

    aded files

---
+++ scripts/gettestimonials.js
@@ -0,0 +1,54 @@
+﻿/// <reference path="jquery-2.1.3.js" />
+/// <reference path="jquery-2.1.3.intellisense.js" />
+
+$(document).ready(function () {
+    console.log("ready");
+
+    $.ajax("/models/datajson.js",
+            { dataType: "json" })
+        .fail(function (jqXHR, textStatus, errorThrown) {
+            console.log("fail " + errorThrown);
+        })
+        .done(function (data, textStatus, jqXHR) {
+            console.log("done");
+            //var jsonData = JSON.stringify(data);
+
+            $.each(data.testimonials, function (index, value) {
+                console.log(value.name);
+            });
+        }
+    );
+
+
+    $.ajax("/models/dataraw.js",
+            { dataType: "script" })
+        .fail(function (jqXHR, textStatus, errorThrown) {
+            console.log("fail " + errorThrown);
+        })
+        .done(function (data, textStatus, jqXHR) {
+            console.log("done");
+            //var jsonData = JSON.stringify(data);
+
+
+            var myarray = [];
+            var myJSONString = "";
+            var myJSONObject;
+            for (var i = 0; i < testimonialsArray.length; i++) {
+                var item = {
+                    "name": testimonialsArray[i][1],
+                    "comment": testimonialsArray[i][3],
+                    "date": testimonialsArray[i][0]
+                };
+
+                myarray.push(item);
+            }
+
+            myJSONString = JSON.stringify({ testimonials: myarray });
+            myJSONObject = JSON.parse(myJSONString);
+            
+            $.each(myJSONObject.testimonials, function (index, value) {
+                console.log(value.name);
+            });
+        }
+    );
+});
